3023f6f1be874061e93c84d9bb1719e844a7ae89
Rafael Costa <rafaelcost@gmail.com>
2026-09-03 09:03:33 -0300

    atualização 03/09/26

---
+++ app/src/main/java/com/rafaelcosta/carteirinhadigital2devest_b/app/session/SessionViewModel.kt
@@ -0,0 +1,20 @@
+package com.rafaelcosta.carteirinhadigital2devest_b.app.session
+
+import androidx.lifecycle.ViewModel
+import com.rafaelcosta.carteirinhadigital2devest_b.feature.login.domain.model.UsuarioLogado
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SessionViewModel: ViewModel(){
+    private val _usuarioLogado = MutableStateFlow<UsuarioLogado?>(null)
+    val usuarioLogado: StateFlow<UsuarioLogado?> = _usuarioLogado.asStateFlow()
+
+    fun setUsuarioLogado(usuario: UsuarioLogado){
+        _usuarioLogado.value=usuario
+    }
+
+    fun limparUsuarioLogado(){
+        _usuarioLogado.value=null
+    }
+}
